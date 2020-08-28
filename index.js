f54e8325b201b0e5a8ada20001b71f4eaeb46f76
AgungMubarok <agungmubarok555@gmail.com>
2020-08-28 16:13:34 +0700

    set env

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const db = require('./config/db');
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 const usersRouter = require('./routes/Users')
 const moviesRouter = require('./routes/Movies')
 const subscriptionsRouter = require('./routes/Subscriptions')
 const historyRouter = require('./routes/HistoryWatch')
+require('dotenv').config()
+
+let PORT = process.env.PORT || 8000
 
 const app = express()
-
+app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -27,6 +30,6 @@ app.use('/', historyRouter)
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => console.log('we re connected'));
 
-app.listen(2000, ()=> {
-    console.log('connected')
+app.listen(PORT, ()=> {
+    console.log(`connected AT PORT ${PORT}`)
 })
